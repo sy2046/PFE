916d9e60d83cc62a0bbd3dfb2fb1d854bacc80d2
Ye Sun <sun.ye@etu.unice.fr>
2016-02-18 16:21:49 +0100

    Wavelet method

--- fBm_wavelet.m
+++ fBm_wavelet.m
@@ -1,21 +1,26 @@
-% Simulation of fBm with wavelet method
+% Simulation of fractional Brownian motion with wavelet method
 
 % Set the sample length 
 lg = 2^15; 
-% Generate and plot wavelet-based fBm for H = 0.3
+
+% Generate wavelet-based fBm for H = 0.3
 fBm03 = wfbm(0.3,lg);
-% Generate and plot wavelet-based fBm for H = 0.5 
+
+% Generate wavelet-based fBm for H = 0.5 
 fBm05 = wfbm(0.5,lg);
-% Generate and plot wavelet-based fBm for H = 0.7 
+
+% Generate wavelet-based fBm for H = 0.7 
 fBm07 = wfbm(0.7,lg);
 
-%plot
+% plot
 figure(1);plot((1:lg)/lg,fBm03);
 title('Simulation of Fractional Brownian motion with Wavelet Method');
 xlabel('Hurst parameter = 0.3');
+
 figure(2);plot((1:lg)/lg,fBm05);
 title('Simulation of Fractional Brownian motion with Wavelet Method');
 xlabel('Hurst parameter = 0.5');
+
 figure(3);plot((1:lg)/lg,fBm07);
 title('Simulation of Fractional Brownian motion with Wavelet Method');
 xlabel('Hurst parameter = 0.7');
